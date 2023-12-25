3aa390568e4683785dac4d386d3fcd81dff70d53
Gilead Odo <odogilead@gmail.com>
2023-12-25 13:55:08 +0100

    Implemented safeAreaView and ScrollView using a custom component. Also made a new tab.

---
+++ constants/dog.ts
@@ -0,0 +1,29 @@
+export const dogs = [
+  {
+    name: "Rottweiler",
+    description: "Rottweiler are very cute",
+    image:
+      "https://images.unsplash.com/photo-1629956737374-f012cb3e875f?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "German Shepherd",
+    description:
+      "The German Shepherd, also known in Britain as an Alsatian, is a German breed of working dog of medium to large size.",
+    image:
+      "https://images.pexels.com/photos/236622/pexels-photo-236622.jpeg?auto=compress&cs=tinysrgb&w=800",
+  },
+  {
+    name: "Bulldog",
+    description:
+      "The Bulldog is a British breed of dog of mastiff type. It may also be known as the English Bulldog or British Bulldog. It is a medium-sized, muscular dog of around 40–55 lb.",
+    image:
+      "https://images.unsplash.com/photo-1521907554502-7440e4702fc3?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Beagle",
+    description:
+      "The beagle is a breed of small scent hound, similar in appearance to the much larger foxhound. The beagle was developed primarily for hunting hare, known as beagling.",
+    image:
+      "https://images.unsplash.com/photo-1505628346881-b72b27e84530?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGJlYWdlbCUyMGRvZ3xlbnwwfHwwfHx8MA%3D%3D",
+  },
+];
